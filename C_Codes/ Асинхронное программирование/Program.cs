e0c1583a40d2dea718d717edd09bdb6453e780c2
ChalkMel <melnikvv24@st.ithub.ru>
2026-05-22 17:58:15 +0300

    7

---
+++ C_Codes/ Асинхронное программирование/Program.cs
@@ -0,0 +1,47 @@
+﻿class Program
+{
+  static async Task Main(string[] args)
+  {
+    string[] urls =
+    [
+      "https://example.com",
+      "https://www.blankwindows.com",
+      "https://radio.garden",
+      "https://longdogechallenge.com"
+    ];
+    
+    List<Task<int>> tasks = new List<Task<int>>();
+
+    foreach (string url in urls)
+    {
+      tasks.Add(GetCharsCount(url));
+    }
+    
+    int[] results = await Task.WhenAll(tasks);
+    
+    Console.WriteLine("Результаты загрузки страниц:");
+    for (int i = 0; i < urls.Length; i++)
+    {
+      Console.WriteLine($"{urls[i]}: {results[i]} видимых символов");
+    }
+  }
+
+  static async Task<int> GetCharsCount(string url)
+  {
+      using (HttpClient client = new HttpClient())
+      {
+        string content = await client.GetStringAsync(url);
+        
+        int visibleCount = 0;
+        foreach (char c in content)
+        {
+          if (!char.IsWhiteSpace(c))
+          {
+            visibleCount++;
+          }
+        }
+                
+        return visibleCount;
+      }
+  }
+}
